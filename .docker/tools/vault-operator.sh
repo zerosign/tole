03144db990ff285ad1695062961dfb8590c2c8a5
zerosign <r1nlx0@gmail.com>
2019-06-19 14:58:08 +0700

    implement vault-operator scripts (status, secrets, root, init)
    - unseal somehow broke or init broke
    - [WIP] auto-init

--- .docker/tools/vault-operator.sh
+++ .docker/tools/vault-operator.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+
+STATUS="000"
+SECRETS=()
+SECRET_PATH=""
+ROOT_TOKEN=""
+
+# check whether $SECRET_PATH variable exists or not
+if [ -z "$SECRET_PATH" ]; then
+    SECRET_PATH=$(pwd)/.docker/volumes/vault/secrets
+fi
+
+# check whether SECRET_PATH exists or not
+# if didn't exist, create it
+if [ ! -d "$SECRET_PATH" ]; then
+    mkdir -p $SECRET_PATH
+fi
 
 # https://www.vaultproject.io/api/system/health.html
 # /sys/health
@@ -8,7 +24,85 @@
 # - 473 if performance standby
 # - 501 if not initialized
 # - 503 if sealed
-
-function check() {
-    vault read
+#
+# return status code only
+#
+function status() {
+    STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$1/v1/sys/health")
 }
+
+#
+# fetch secrets from files
+#
+function secrets() {
+    SECRETS=($(cat $SECRET_PATH/secret.keys))
+}
+
+function root() {
+    secrets
+    ROOT_TOKEN=${SECRETS[7]}
+}
+
+#
+# unseal vault from secret.keys
+#
+function unseal() {
+    vault operator unseal -address=$1 ${SECRETS[0]}
+    vault operator unseal -address=$1 ${SECRETS[1]}
+    vault operator unseal -address=$1 ${SECRETS[2]}
+}
+
+#
+# call vault operator init then dump
+# secrets into /secrets/secret.keys
+#
+# formats (separated by newline) :
+# secrets[0]
+# secrets[1]
+# ...
+# root_token
+#
+function init() {
+    vault operator init -address=$1 | awk -F':' '{ if ($2 != "") { gsub(/[ \\t]+/, "", $2); print $2 } }' > \
+                                          "$SECRET_PATH/secret.keys"
+}
+
+# function auto-init() {
+#     # init if only vault aren't being intialized
+#     local status =''
+#     status = status()
+#     if [ status = "501" ]; then
+#         init()
+#         unseal()
+#     fi
+
+#     if [ status = "503" ]; then
+
+#     fi
+# }
+
+case $1 in
+    status)
+        status $2
+        echo $STATUS
+        ;;
+    secrets)
+        secrets
+        printf '%s\n' "${SECRETS[@]}"
+        ;;
+    init)
+        init $2
+        ;;
+    unseal)
+        secrets
+        unseal $2 $SECRETS
+        ;;
+    auto-init)
+        auto-init
+        ;;
+    help)
+        echo -n "vault-operator [status|secrets|init|unseal|auto-init]"
+        ;;
+    *)
+        echo -n "unknown action"
+esac
--- .docker/volumes/vault-etcd/config/vault.hcl
+++ .docker/volumes/vault-etcd/config/vault.hcl
@@ -1,5 +1,5 @@
 storage "etcd" {
-  address = "etcd-grpc-proxy:2379"
+  address = "http://etcd0:2379,http://etcd1:2379,http://etcd2:2379"
   path = "/vault/"
   etcd_api = "v3"
 }
