0717dbdced0f89dfbe38d4f75dd4f6b6672861c2
zerosign <r1nlx0@gmail.com>
2019-06-19 17:06:27 +0700

    add .dockerignore for etcd volumes
    - [WIP] implement auto policy init in vaults ([0-9]+\.(a-z)+\.hcl)
    - add a way to check whether policy already exists or not
    - local workflow for easily having sources container up & running :
      - `make clean-compose` (this will reset etcd)
      - `make build-compose` (this will build etcd, consul, vault
         containers in locals)
      - vault - docker-compose up vault-etcd

--- .docker/tools/vault-operator.sh
+++ .docker/tools/vault-operator.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+SECRET_PATH=$SECRET_PATH
+POLICY_PATH=$POLICY_PATH
 STATUS="000"
 SECRETS=()
-SECRET_PATH=""
 ROOT_TOKEN=""
+TOKEN=""
 
 # check whether $SECRET_PATH variable exists or not
 if [ -z "$SECRET_PATH" ]; then
@@ -15,6 +17,16 @@ fi
 if [ ! -d "$SECRET_PATH" ]; then
     mkdir -p $SECRET_PATH
 fi
+
+
+if [ -z "$POLICY_PATH" ]; then
+    POLICY_PATH=$(pwd)/.docker/volumes/vault/policies
+fi
+
+if [ ! -d "$POLICY_PATH" ]; then
+    mkdir -p $POLICY_PATH
+fi
+
 
 # https://www.vaultproject.io/api/system/health.html
 # /sys/health
@@ -52,6 +64,11 @@ function unseal() {
     vault operator unseal -address=$1 ${SECRETS[2]}
 }
 
+function login() {
+    vault login -address=$1 $2
+    TOKEN=$(vault token create -address=$1 -format=json | jq -r '.auth.client_token ' -c)
+}
+
 #
 # call vault operator init then dump
 # secrets into /secrets/secret.keys
@@ -63,23 +80,39 @@ function unseal() {
 # root_token
 #
 function init() {
-    vault operator init -address=$1 | awk -F':' '{ if ($2 != "") { gsub(/[ \\t]+/, "", $2); print $2 } }' > \
-                                          "$SECRET_PATH/secret.keys"
+    # vault operator init -address=$1 > $SECRET_PATH/output.raw
+    # cat $SECRET_PATH/output.raw | awk -F':' '{ if ($2 != "") { gsub(/[ \t]+/, "", $2); print $2 } }' > "$SECRET_PATH/secret.keys"
+    vault operator init -address=$1 | awk -F':' '{ if ($2 != "") { gsub(/[ \t]+/, "", $2); print $2 } }' > "$SECRET_PATH/secret.keys"
 }
 
-# function auto-init() {
-#     # init if only vault aren't being intialized
-#     local status =''
-#     status = status()
-#     if [ status = "501" ]; then
-#         init()
-#         unseal()
-#     fi
+function policy-init() {
+    POLICES=($(find $POLICY_PATH -name "*.hcl" -type f))
+    for path in "${POLICES[@]}"; do
+        echo $path
 
-#     if [ status = "503" ]; then
+    done
+}
 
-#     fi
-# }
+function policy-exists() {
+    POLICY_EXIST=$(curl -s -o /dev/null -w "%{http_code}" -H "X-Vault-Token: $(vault print token)" $1/v1/sys/policies/acl/$2)
+}
+
+function auto-init() {
+    status $1
+
+    case $STATUS in
+        "503")
+            unseal $1
+            ;;
+        "501")
+            init $1
+            secrets
+            unseal $1 $SECRETS
+            ;;
+        *)
+            echo -n "everything is good :))"
+    esac
+}
 
 case $1 in
     status)
@@ -93,12 +126,18 @@ case $1 in
     init)
         init $2
         ;;
+    login)
+        root
+        login $2 $ROOT_TOKEN
+        ;;
+    policy-init)
+        ;;
     unseal)
         secrets
         unseal $2 $SECRETS
         ;;
     auto-init)
-        auto-init
+        auto-init $2
         ;;
     help)
         echo -n "vault-operator [status|secrets|init|unseal|auto-init]"
